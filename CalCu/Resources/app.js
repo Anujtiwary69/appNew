9fe7f4eefda34864485703e29609eb84613f1aac
anujtiwary69 <anujtiwary69@gmail.com>
2017-07-06 15:24:20 +0400

    updater

--- CalCu/Resources/app.js
+++ CalCu/Resources/app.js
@@ -8,16 +8,20 @@ var view01 = Ti.UI.createView({
   	 backgroundColor:'white',
      width:"100%",
      height:"50%",
-     layout:"horizontal"
+     layout:"horizontal",
+      textAlign: Ti.UI.TEXT_ALIGNMENT_RIGHT,
 	
 });
 var view02 = Ti.UI.createView({
 
-       width:"100%",
-       height:"50%",
+       width:"99.5%",
+       height:"100%",
        layout:"horizontal",
-       left:15,
-       right:15
+       left:20,
+       right:0,
+       textAlign: Ti.UI.TEXT_ALIGNMENT_RIGHT,
+       
+       
 	
 });
  var label = Ti.UI.createLabel({
@@ -25,30 +29,104 @@ var view02 = Ti.UI.createView({
  	color:'black',
  	top:"30%",
  	left:"10",
- 	 font: {fontFamily:'Snell Roundhand', fontSize:36},
+ 	textAlign: Ti.UI.TEXT_ALIGNMENT_RIGHT,
+ 	 width: Ti.UI.SIZE, height: Ti.UI.SIZE,
+ 	 font: {fontFamily:'Snell Roundhand', fontSize:"56dp"},
         shadow:{color:'gray', offset:{width:1,height:1}}
  });
- for(i=0;i<10;i++)
+ var Button = [];
+ for(i=0;i<15;i++)
  {
- 	var Button =Ti.UI.createButton({
- 	 title: i,
-     width: 100,
-     height: 50,
-     backgroundColor:'#ea2d6d',
-     left:10,
-     bottom:10
- });
+ 	if(i<10)
+ 	{
+ 		Button =Ti.UI.createButton({
+	 	    title: i,
+	        width: 50,
+	     	height: 50,
+	     	backgroundColor:'#ea2d6d',
+	     	left:10,
+	     	bottom:10,
+	     	value : i
+     	 });
+ 	}
+ 	else if(i==10){
+ 		Button =Ti.UI.createButton({
+	 	    title: "C",
+	        width: 50,
+	     	height: 50,
+	     	backgroundColor:'blue',
+	     	left:10,
+	     	bottom:10,
+	     	value : "c"
+     	 });
+ 	}
+ 	else if(i==11){
+ 		Button =Ti.UI.createButton({
+	 	    title: "+",
+	        width: 50,
+	     	height: 50,
+	     	backgroundColor:'#ea2d6d',
+	     	left:10,
+	     	bottom:10,
+	     	value : "+"
+     	 });
+ 	}
+ 	else if(i==12){
+ 		Button =Ti.UI.createButton({
+	 	    title: "-",
+	        width: 50,
+	     	height: 50,
+	     	backgroundColor:'#ea2d6d',
+	     	left:10,
+	     	bottom:10,
+	     	value : "-"
+     	 });
+ 	}
+ 	 	else if(i==13){
+ 		Button =Ti.UI.createButton({
+	 	    title: "/",
+	        width: 50,
+	     	height: 50,
+	     	backgroundColor:'#ea2d6d',
+	     	left:10,
+	     	bottom:10,
+	     	value : "/"
+     	 });
+ 	}
+ 	
+ 	 	else if(i==14){
+ 		Button =Ti.UI.createButton({
+	 	    title: "*",
+	        width: 50,
+	     	height: 50,
+	     	backgroundColor:'#ea2d6d',
+	     	left:10,
+	     	bottom:10,
+	     	value : "x"
+     	 });
+ 	}
+ 	
+ 	
+ 	
+ 	 
+
+  Button.addEventListener("click", function(e) {
+  	if(this.value=='c')
+  	{
+  		label.text="0";
+  	}
+  	else
+  	{
+  		label.text+=this.value;
+  	}
+});
  view02.add(Button);
  }
+
  
 window.open();
 view01.add(label);
 view02.add(Button);
-Button.addEventListener('click', function(e) {
-	// code here is run when the event is fired
-	// properties of the event object 'e' describe the event and object that received it
-	alert();
-});
 window.add(view01);
 window.add(view02);
 
